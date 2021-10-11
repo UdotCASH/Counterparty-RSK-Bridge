0af83d165011bbf137cfbcd0782a13636344b6cf
hfccr <hfccr@outlook.com>
2021-10-12 04:11:27 +0530

    Refactor from bridge scripts

--- scripts/bot/processSends.js
+++ scripts/bot/processSends.js
@@ -1,32 +1,34 @@
 const { ethers } = require('ethers');
 const { walletPrivateKey } = require('./../smartXCP/privateKey');
+const { XCP_WALLET, getSends } = require('./../xcp');
 
-async function processSends(smartXCP, XCP_Address) {
-  const { result: sends } = await getSends(XCP_Address);
-
-  //console.log(sends)
+async function processSends(smartXCP) {
+  console.log('Process Sends');
+  const { result: sends } = await getSends(XCP_WALLET);
+  console.log(sends);
   for (send of sends) {
-    let isminted = await smartXCP.is_XCP_deposit_minted(send.tx_hash);
-    amount = ethers.utils.parseUnits(send.quantity.toString(), 10);
+    console.log('send', send);
+    const { tx_hash: txHash, source, quantity, memo_hex: memoHex } = send;
+    const isMinted = await smartXCP.is_XCP_deposit_minted(txHash);
+    console.log('isMinted', isMinted);
+    const amount = ethers.utils.parseUnits(quantity.toString(), 10);
     console.log(ethers.utils.formatUnits(amount, 0));
-    if (!isminted) {
+    if (!isMinted) {
       let destinationAddress;
       try {
-        destinationAddress = await ethers.utils.getAddress(send.memo_hex);
+        destinationAddress = await ethers.utils.getAddress(memoHex);
       } catch {
         destinationAddress = walletPrivateKey.address;
       }
-      console.log(destinationAddress);
-      amount = ethers.utils.parseUnits(send.quantity.toString(), 10);
-      console.log(amount);
+      console.log('destinationAddress', destinationAddress);
       try {
-        let tx = await smartXCP.mint(
-          send.tx_hash,
-          send.source,
+        const tx = await smartXCP.mint(
+          txHash,
+          source,
           amount,
           destinationAddress
         );
-        let conf = await tx.wait();
+        const conf = await tx.wait();
         console.log(conf);
       } catch {
         console.log('Already minted');
@@ -34,3 +36,5 @@ async function processSends(smartXCP, XCP_Address) {
     }
   }
 }
+
+module.exports = processSends;
